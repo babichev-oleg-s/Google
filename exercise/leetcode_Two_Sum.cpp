622702f2afcf9b97414d3fdd7221c7c5c4f0f7a0
babichev-oleg-s <60172046+babichev-oleg-s@users.noreply.github.com>
2020-01-25 17:11:50 +0300

    Update leetcode_Two_Sum.cpp

--- exercise/leetcode_Two_Sum.cpp
+++ exercise/leetcode_Two_Sum.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> dic;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            int numberToFind = target - nums[i];
+            if (dic.find(numberToFind) != dic.end()) {
+                res.push_back(dic[numberToFind]);
+                res.push_back(i);
+                return res;
+            }
+            dic[nums[i]] = i;
+        }
+        return res;
+    }
+};
