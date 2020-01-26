7efb4b37237ea6939479188e28cd567643ffa1d9
babichev-oleg-s <60172046+babichev-oleg-s@users.noreply.github.com>
2020-01-26 17:46:25 +0300

    Update leetcode.com_Power_of_Two.cpp

--- exercise/leetcode.com_Power_of_Two.cpp
+++ exercise/leetcode.com_Power_of_Two.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       long long i=1;
+        while(i<n) {
+            
+            i*=2;
+            
+        }
+        
+        return i==n;
+    }
+};
