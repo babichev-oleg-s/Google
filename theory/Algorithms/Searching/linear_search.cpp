39973cc9c69f6b2418dc75f9637ca011f47e1368
babichev-oleg-s <60172046+babichev-oleg-s@users.noreply.github.com>
2020-02-02 20:44:15 +0300

    Update linear_search.cpp

--- theory/Algorithms/Searching/linear_search.cpp
+++ theory/Algorithms/Searching/linear_search.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int search(int arr[], int n, int x);
+
+int main()
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 40;
+    int n = sizeof(arr) / sizeof(arr[0]);
+         int j=0;
+         cout << "Array  " ;
+         while(j<=n-1) {
+            cout <<arr[j] << " ";
+            j++;
+         }
+
+   int result = search(arr, n, x);
+   (result == -1)? cout<<"Element is " << x <<" not present in array"
+                 : cout<<"Element is " << x <<" present at index " <<result;
+    return 0;
+}
+
+
+int search(int arr[], int n, int x)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
 
